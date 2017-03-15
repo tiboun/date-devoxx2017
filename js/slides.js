3ccba537b4c654131292197f703d20d0242935c4
bkhampho <bkhampho@apec.fr>
2017-03-15 17:59:22 +0100

    initialisation des slides à partir de ceux de devoxx

---
+++ js/slides.js
@@ -0,0 +1,45 @@
+Reveal.addEventListener('epoch', function(event) {
+    if(!this.initialized) {
+        var updateTime = function(){
+            document.getElementById("timeElapsed").innerHTML = Date.now();
+        };
+        updateTime();
+        window.setInterval(
+            updateTime,
+            111
+        );
+    }
+    this.initialized = true;
+});
+
+Reveal.addEventListener('timezone', function(event) {
+    if(!this.initialized) {
+        var getUTCOffset = function(timezone) {
+            var offset = moment().tz(timezone).utcOffset();
+            var offsetPrefix = offset < 0 ? '-': '+';
+            offset = Math.abs(offset);
+            var hours = _.padStart(Math.floor(offset/60), 2, '0');
+            var minutes = _.padStart(offset % 60, 2, '0');
+            return offsetPrefix + hours + ':' + minutes;
+        };
+        var getTime = function(timezone) {
+            return moment().tz(timezone).format('DD/MM/YYYY HH:mm:ss');
+        };
+        document.getElementById("newYorkTZ").innerText = getUTCOffset('America/New_York');
+        document.getElementById("londonTZ").innerText = getUTCOffset('Europe/London');
+        document.getElementById("parisTZ").innerText = getUTCOffset('Europe/Paris');
+        document.getElementById("indiaTZ").innerText = getUTCOffset('Asia/Kolkata');
+        var updateTime = function(){
+            document.getElementById("newYorkTime").innerText = getTime('America/New_York');
+            document.getElementById("londonTime").innerText = getTime('Europe/London');
+            document.getElementById("parisTime").innerText = getTime('Europe/Paris');
+            document.getElementById("indiaTime").innerText = getTime('Asia/Kolkata');
+        };
+        updateTime();
+        window.setInterval(
+            updateTime,
+            1000
+        );
+    }
+    this.initialized = true;
+});
